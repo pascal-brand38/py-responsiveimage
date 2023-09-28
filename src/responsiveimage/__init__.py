ca1a70dbfe37d643d2140e3ad96ef3b9cd24bfb8
Pascal Brand <pascal.brand38@gmail.com>
2023-09-28 18:08:45 +0200

    Hello world
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

---
+++ src/responsiveimage/__init__.py
@@ -0,0 +1,2 @@
+# from https://packaging.python.org/en/latest/tutorials/packaging-projects/
+#   __init__.py is required to import the directory as a package, and should be empty.
---
+++ src/responsiveimage/__main__.py
@@ -0,0 +1,18 @@
+'''
+MIT License
+Copyright (c) 2023 Pascal Brand
+
+TODO
+'''
+
+from responsiveimage import create
+
+
+def main():
+  """
+  Main function of python package responsive-image
+  """
+  create()
+
+if __name__ == "__main__":
+  main()
